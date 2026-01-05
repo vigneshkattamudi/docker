a2aa3297fe753f5c3bfb47dda12654cdecf4c302
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2026-01-05 22:10:40 +0000

    docker

--- docker.sh
+++ docker.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+set -euxo pipefail
 
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
+dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl daemon-reexec
+systemctl enable --now docker
+
 usermod -aG docker ec2-user
 
-growpart /dev/nvme0n1 4
-lvextend -L +20G /dev/RootVG/rootVol
-lvextend -L +10G /dev/RootVG/varVol
+if lsblk /dev/nvme0n1p4; then
+  growpart /dev/nvme0n1 4
+  lvextend -L +20G /dev/RootVG/rootVol
+  lvextend -L +10G /dev/RootVG/varVol
+  xfs_growfs /
+  xfs_growfs /var
+fi
 
-xfs_growfs /
-xfs_growfs /var
+reboot

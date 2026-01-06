3b4e63098112b4e47acc91d08c854af40c8c5ccf
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2026-01-06 21:22:00 +0000

    docker

--- docker.sh
+++ docker.sh
@@ -18,4 +18,20 @@ if lsblk /dev/nvme0n1p4; then
   xfs_growfs /var
 fi
 
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
+
+eksctl version
+kubectl version
+
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
 reboot

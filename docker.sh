0afd871ca49853b78a6f771611e3447ae84f8d2e
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2026-01-08 09:23:06 +0000

    docker

--- docker.sh
+++ docker.sh
@@ -55,45 +55,14 @@ ln -sf /opt/kubectx/kubens /usr/local/bin/kubens
 ln -sf /opt/kubectx/kubectx /usr/local/bin/kubectx
 
 # -----------------------------
+# k9s installation
+# -----------------------------
+curl -sS https://webinstall.dev/k9s | bash
+
+# Ensure k9s is on PATH for all users
+export PATH="$HOME/.local/bin:$PATH"
+
+# -----------------------------
 # Reboot to finalize
 # -----------------------------
 reboot
-
-
-#========================================================================================================================
-
-# set -euxo pipefail
-
-# dnf -y install dnf-plugins-core
-# dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-# dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# systemctl daemon-reexec
-# systemctl enable --now docker
-# usermod -aG docker ec2-user
-
-# if lsblk /dev/nvme0n1p4; then
-#   growpart /dev/nvme0n1 4
-#   lvextend -L +20G /dev/RootVG/rootVol
-#   lvextend -L +10G /dev/RootVG/varVol
-#   xfs_growfs /
-#   xfs_growfs /var
-# fi
-
-# ARCH=amd64
-# PLATFORM=$(uname -s)_$ARCH
-# curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-# tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-# install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
-
-# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
-# chmod +x ./kubectl
-# mv kubectl /usr/local/bin/kubectl
-
-# eksctl version
-# kubectl version
-
-# git clone https://github.com/ahmetb/kubectx /opt/kubectx
-# ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-
-# reboot
